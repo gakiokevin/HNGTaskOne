7c90aca3bc9a3c73f44c1212527f926d19322659
gakiokevin <gakiokevin5@gmail.com>
2023-09-09 18:34:13 +0300

    compeleted task 1

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-//defining an array containing all days of the week
+//Function to update the time to display the  current time
+ updateTime()
+function updateTime(){
 let daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday','Saturday']
 let date = new Date().getDay()
 let weekDay = daysOfWeek[date]
  let dayOfWeek = document.getElementById('dayOfWeek')
  dayOfWeek.textContent =  weekDay
-
-
-
+   //defining the UTC time in seconds
  let UTC = new Date().getTime()
- let UTCTime = document.getElementById('UTCTime')
- UTCTime.textContent = UTC
+   let UTCTime = document.getElementById('UTCTime')
+   UTCTime.textContent = UTC
+}
+setInterval(updateTime,1000)
